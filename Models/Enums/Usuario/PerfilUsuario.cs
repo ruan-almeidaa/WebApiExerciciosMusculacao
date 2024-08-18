6aa431e939b637c34b64e6dd80d52b0b2180db18
Ruan <ruanalmeidawegner@gmail.com>
2024-08-18 10:45:18 -0300

    criando entidades

---
+++ Models/Enums/Usuario/PerfilUsuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Enums.Usuario
+{
+    public enum PerfilUsuario
+    {
+        [Description("Padrao")]
+        Padrao = 0,
+        [Description("Administrador")]
+        Administrador = 1
+    }
+}
