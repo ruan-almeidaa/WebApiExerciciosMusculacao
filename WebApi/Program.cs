4379e0a1581bae04421dad72ae252dd157a5a92f
Ruan <ruanalmeidawegner@gmail.com>
2024-08-18 12:56:29 -0300

    Conexão com banco de dados

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,5 @@
+using Infra.Database;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Entity Framework
+builder.Services.AddDbContext<BancoContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Database"), sqlServerOptions =>
+    {
+        sqlServerOptions.CommandTimeout(60); // Tempo limite de comando em segundos
+        sqlServerOptions.EnableRetryOnFailure(1); // Tenta novamente 1 vez em caso de falha
+    }));
 
 var app = builder.Build();
 
