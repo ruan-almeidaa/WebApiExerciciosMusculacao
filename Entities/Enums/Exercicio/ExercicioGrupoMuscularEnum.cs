50b847d410c28c50d015c0fdf20498a2ab9e9bbb
Ruan <ruanalmeidawegner@gmail.com>
2024-08-18 19:56:11 -0300

    Ajustado enum de grupos musculares.

--- Entities/Enums/Exercicio/ExercicioGrupoMuscularEnum.cs
+++ Entities/Enums/Exercicio/ExercicioGrupoMuscularEnum.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +9,37 @@ namespace Entities.Enums.Exercicio
 {
     public enum ExercicioGrupoMuscularEnum
     {
-        Peito,
+        [Description("Peito")]
+        Peito = 1,
+
+        [Description("Costas")]
         Costas,
+
+        [Description("Ombros")]
         Ombros,
+
+        [Description("Bíceps")]
         Biceps,
+
+        [Description("Tríceps")]
         Triceps,
+
+        [Description("Quadríceps")]
         Quadriceps,
+
+        [Description("Posterior de Coxa")]
         PosteriorCoxa,
+
+        [Description("Glúteos")]
         Gluteos,
+
+        [Description("Panturrilha")]
         Panturrilha,
-        Abdomen
+
+        [Description("Abdômen")]
+        Abdomen,
+
+        [Description("Trapézio")]
+        Trapezio
     }
 }
