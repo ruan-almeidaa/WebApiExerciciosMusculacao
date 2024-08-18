0bc8eb649a6d30699034f1fa70dd6a87531cd9a8
Ruan <ruanalmeidawegner@gmail.com>
2024-08-18 10:45:18 -0300

    criando entidades

---
+++ Models/Entities/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Enums.Usuario;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public required string Email { get; set; }
+        public required string Nome { get; set; }
+        public required string Sobrenome { get; set; }
+        public required PerfilUsuario perfilUsuario { get; set; }
+    }
+}
